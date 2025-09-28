89717b2d1704d286285d88158160c2694a1d5c26
GrayM8 <matthew.gray.marshall@gmail.com>
2025-09-27 19:16:10 -0500

    Added google content host for OAuth profile pictures

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "www.longhornsimracing.org" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
       // (optional) lock to your account path:
       // { protocol: "https", hostname: "res.cloudinary.com", pathname: "/YOUR_CLOUD_NAME/**" },
     ],
