0eac8725573173857cb0e1e102993eb8f8a9ac2b
GrayM8 <matthew.gray.marshall@gmail.com>
2025-12-30 21:18:34 -0600

    Removed Rick Astley

--- src/lib/gallery.ts
+++ src/lib/gallery.ts
@@ -19,5 +19,4 @@ export type GalleryItem = {
 export const galleryItems: GalleryItem[] = [
   { type: "youtube", src: "https://www.youtube.com/embed/1G0uj-3s12o", title: "YouTube video player" },
   { type: "googledrive", src: "https://drive.google.com/file/d/14A8G21BfeFI8IFPnRRUxvCgDP6H9W5s4/preview", title: "Google Drive video player" },
-  { type: "youtube", src: "https://www.youtube.com/embed/dQw4w9WgXcQ", title: "YouTube video player" },
 ];
